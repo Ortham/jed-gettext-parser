dc339db8b8d306bbe7556cf14b729efe985d2410
WrinklyNinja <wrinklyninja1@gmail.com>
2014-09-09 16:14:53 +0100

    Set up Grunt + Mocha for browser testing.
    Adapted from https://saucelabs.com/javascript/mocha-js

---
+++ gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+    var browsers = [{
+        browserName: "firefox",
+        version: "31",
+        platform: "Windows 7"
+    }, {
+        browserName: "chrome",
+        platform: "Windows 7",
+        version: "beta"
+    }, {
+        browserName: "chrome",
+        platform: "Windows 7",
+        version: "35"
+    }, {
+        browserName: "chrome",
+        platform: "Windows 7",
+        version: "36"
+    }, {
+        browserName: "chrome",
+        platform: "Windows 7"
+    }, {
+        browserName: "internet explorer",
+        platform: "Windows 7",
+        version: "8"
+    }, {
+        browserName: "internet explorer",
+        platform: "Windows 7",
+        version: "9"
+    }, {
+        browserName: "internet explorer",
+        platform: "Windows 7",
+        version: "10"
+    }, {
+        browserName: "internet explorer",
+        platform: "Windows 7",
+        version: "11"
+    }, {
+        browserName: "safari",
+        platform: "OS X 10.9",
+        version: "7"
+    }];
+
+    grunt.initConfig({
+        connect: {
+            server: {
+                options: {
+                    base: "",
+                    port: 9999
+                }
+            }
+        },
+        'saucelabs-mocha': {
+            all: {
+                options: {
+                    urls: ["http://127.0.0.1:9999/test/browser/index.html"],
+                    build: process.env.TRAVIS_JOB_ID,
+                    throttled: 3,
+                    browsers: browsers,
+                    testname: "mocha tests",
+                    tags: ["master"]
+                }
+            }
+        },
+        watch: {}
+    });
+
+    // Loading dependencies
+    for (var key in grunt.file.readJSON("package.json").devDependencies) {
+        if (key !== "grunt" && key.indexOf("grunt") === 0) grunt.loadNpmTasks(key);
+    }
+
+    grunt.registerTask("dev", ["connect", "watch"]);
+    grunt.registerTask("test", ["connect", "saucelabs-mocha"]);
+};
--- test/get-files.sh
+++ test/get-files.sh
@@ -7,3 +7,15 @@ wget http://i18n.svn.wordpress.org/ru_RU/trunk/messages/ru_RU.mo
 wget http://i18n.svn.wordpress.org/ru_RU/trunk/messages/ru_RU.po
 msgconv --output-file=ru_RU_cp1251.po --to-code=CP1251 ru_RU.po
 msgfmt --output-file=ru_RU_cp1251.mo ru_RU_cp1251.po
+
+# Get Mocha, Jed and should.js for browser tests, since I don't think they'll
+# have the browser code in their npm packages.
+wget -P test/browser https://raw.githubusercontent.com/shouldjs/should.js/7f7bbe0a104fcbe169d9e4176e6b460ee4b94ec2/should.js
+wget -P test/browser https://raw.githubusercontent.com/SlexAxton/Jed/d18499bc7f609720257a9a2e601e3cfe9516db95/jed.js
+wget -P test/browser https://raw.githubusercontent.com/visionmedia/mocha/bfb4ec14f0403450e4c6a7ff190eb0596880edcc/mocha.css
+wget -P test/browser https://raw.githubusercontent.com/visionmedia/mocha/bfb4ec14f0403450e4c6a7ff190eb0596880edcc/mocha.js
+
+# Also copy gettext files into browser directory.
+for file in ru_RU.mo ru_RU.po ru_RU_cp1251.mo ru_RU_cp1251.po; do
+    cp $file ./test/browser/$file
+done
