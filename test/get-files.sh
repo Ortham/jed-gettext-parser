1018edc68f89bbb0b2de9a8ec9c1c4fca103eddf
Oliver Hamlet <oliver.hamlet@gmail.com>
2014-11-04 14:02:22 +0000

    Minor tidy of file fetching script.

--- test/get-files.sh
+++ test/get-files.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-# Get a set of mo files to test with.
-
+# Get an mo file to test with.
 wget http://i18n.svn.wordpress.org/ru_RU/trunk/messages/ru_RU.mo
 
 # Also create a non-UTF-8 version to test with.
 wget http://i18n.svn.wordpress.org/ru_RU/trunk/messages/ru_RU.po
 msgconv --output-file=ru_RU_cp1251.po --to-code=CP1251 ru_RU.po
 msgfmt --output-file=ru_RU_cp1251.mo ru_RU_cp1251.po
+rm ru_RU.po ru_RU_cp1251.po
+
+# Also copy gettext files into browser directory.
+for file in ru_RU.mo ru_RU_cp1251.mo; do
+    cp $file ./test/browser/$file
+done
 
 # Get Mocha, Jed, should.js and Encoding API and Promises polyfills for browser
 # tests.
@@ -17,8 +22,3 @@ wget -P test/browser https://raw.githubusercontent.com/inexorabletash/text-encod
 wget -P test/browser http://s3.amazonaws.com/es6-promises/promise-1.0.0.min.js
 wget -P test/browser https://raw.githubusercontent.com/visionmedia/mocha/1.21.4/mocha.css
 wget -P test/browser https://raw.githubusercontent.com/visionmedia/mocha/1.21.4/mocha.js
-
-# Also copy gettext files into browser directory.
-for file in ru_RU.mo ru_RU.po ru_RU_cp1251.mo ru_RU_cp1251.po; do
-    cp $file ./test/browser/$file
-done
